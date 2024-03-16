e7868b7addc8ee5e9d09c4c37bb9c00c6de9a665
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-03-17 01:48:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ class Config(object):
     TECH_VJ_UPDATES_CHANNEL = int(tech_vj_update_channel) if tech_vj_update_channel and id_pattern.search(tech_vj_update_channel) else None  
     
     # Url Shortner Information 
-    TECH_VJ = bool(environ.get('TECH_VJ', False)) # Set False If you want shortlink off else True
+    TECH_VJ = bool(environ.get('TECH_VJ', True)) # Set False If you want shortlink off else True
     TECH_VJ_URL = environ.get('TECH_VJ_URL', 'link.vipurl.in') # your shortlink url domain or url without https://
     TECH_VJ_API = environ.get('TECH_VJ_API', 'b448ce021d547cc49ea844f03feaa6c988231af6') # your url shortner api
     TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "https://t.me/How_To_Open_Linkl")
