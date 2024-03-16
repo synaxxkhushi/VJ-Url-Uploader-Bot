f0cf850db15f871caf6ec676016a4048f13df15d
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-03-17 01:48:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -63,8 +63,8 @@ class Config(object):
     
     # Url Shortner Information 
     TECH_VJ = bool(environ.get('TECH_VJ', False)) # Set False If you want shortlink off else True
-    TECH_VJ_URL = environ.get('TECH_VJ_URL', 'moneykamalo.com') # your shortlink url domain or url without https://
-    TECH_VJ_API = environ.get('TECH_VJ_API', '0eefb93e1e3ce9470a7033115ceb1bad13a9d674') # your url shortner api
+    TECH_VJ_URL = environ.get('TECH_VJ_URL', 'link.vipurl.in') # your shortlink url domain or url without https://
+    TECH_VJ_API = environ.get('TECH_VJ_API', 'b448ce021d547cc49ea844f03feaa6c988231af6') # your url shortner api
     TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "https://t.me/How_To_Open_Linkl")
 
 
